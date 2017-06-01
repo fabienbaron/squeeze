1291562e047f9c7cc5971997b825ad326f5ff623
Fabien Baron <baron@chara.gsu.edu>
2017-06-01 17:01:52 -0400

    Improved recursive julia

--- JULIA/squeeze_iterative.jl
+++ JULIA/squeeze_iterative.jl
@@ -2,18 +2,20 @@ include("oiplot.jl")
 include("chain_average.jl")
 # Squeeze iterative procedure
 dataname = "../sample_data/2004-data1.oifits";
-npixs = [16, 32, 64];
-pixellations = [1.2, 0.6, 0.3];
-elements = [200, 500, 1000];
+
+maxit = 4
+npixs = [16, 32, 64, 128];
+pixellations = [1.2, 0.6, 0.3, 0.15];
+elements = [200, 500, 1000, 4000];
 basedir = "../fake/";
 nchains = 10;
 
-i=1
 
-for i=1:3
+for i=1:maxit
   npix = npixs[i];
   pixellation=pixellations[i];
   element = elements[i];
+  mkdir(string(basedir,npix));
   outputbase = string(basedir, npix,"/reconst")
   if(i==1)
     run(`../bin/squeeze $dataname -w $npix -s $pixellation -e $element -o $outputbase -chains $nchains`)
@@ -23,9 +25,9 @@ for i=1:3
   end
 
   avg, err = chain_average(outputbase)
-  if (i<3)
+  if (i<maxit)
     avgx2 = gridx2(avg);
     f = FITS(string(basedir,"start",npixs[i+1], ".fits"), "w"); write(f, avgx2); close(f);
   end
-
+  imdisp(avg)
 end
